dbbd10ceda68dccc54f14e8c425eb0357ad147c6
badr21b <badreddine@pembepanjur.com>
2018-08-12 20:38:30 +0300

    First commit

---
+++ app/home.php
@@ -0,0 +1,18 @@
+<?php
+$test = "hello world";
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 05/08/18
+ * Time: 14:16
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First test</h1>
+<div><?php echo $test ?></div>
+</body>
+</html>
---
+++ html/home.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 05/08/18
+ * Time: 14:17
+ */
